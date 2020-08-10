7baf145546b302c2407e8523af85549403b5508e
siddharth-I <67158060+siddharth-I@users.noreply.github.com>
2020-08-10 18:33:39 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Movie List Application! There are 10 movies in this list. Please enter the category you are interested in: animated, drama, horror, or scifi. ");
+            var userInput = Console.ReadLine();
+        
+            var result = new Movie(Title, Category);
+            Console.WriteLine($"The movies you selected are ({result.Title}, {result.Category})");
+        }
+    }
+}
