937961b7a6da61aaf1cc920f898e02911c835fd3
siddharth-I <67158060+siddharth-I@users.noreply.github.com>
2020-08-10 18:34:42 -0400

    Add files via upload

---
+++ Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab11
+{
+    class Movie
+    {
+        public static List<Movie> MovieList()
+        {
+            List<Movie> movies = new List<Movie>();
+            movies.Add(new Movie("The Lorax", "animated"));
+            movies.Add(new Movie("Lego Movie", "animated"));
+            movies.Add(new Movie("Your Name", "animated"));
+            movies.Add(new Movie("Summer Wars", "animated"));
+            movies.Add(new Movie("1917", "drama"));
+            movies.Add(new Movie("Midway", "drama"));
+            movies.Add(new Movie("Alien", "horror"));
+            movies.Add(new Movie("Predator", "horror"));
+            movies.Add(new Movie("Star Wars", "scifi"));
+            movies.Add(new Movie("Godzilla", "scifi"));
+            return movies;
+        }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public Movie(string title, string category)
+        {
+            Title = title;
+            Category = category;
+        }
+    }
+}
